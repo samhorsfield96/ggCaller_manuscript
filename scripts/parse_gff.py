ea444b80934738eeaabb6896485347c20190213e
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2023-06-12 12:01:13 +0100

    Makes peppan file optional.

--- scripts/parse_gff.py
+++ scripts/parse_gff.py
@@ -10,8 +10,8 @@ def get_options():
 
 	# input options
 	parser.add_argument('--peppan',
-						required=True,
-						help='PEPPAN .gff file to analysis, or pair of PEPPAN .genes and .encode.csv in format "x.genes,x.encode.csv"')
+						default=None,
+						help='PEPPAN .gff file to analysis')
 	parser.add_argument('--roary',
 						default=None,
 						help='Roary clusters matrix file to analysis')
@@ -144,103 +144,98 @@ if __name__ == "__main__":
 	ignore_pseudogenes = options.ignore_pseudogenes
 	verbose = options.verbose
 
-	if "," in peppan_infile:
-		split_peppan = peppan_infile.split(",")
-		peppan_genes = split_peppan[0]
-		peppan_encode = split_peppan[1]
-		peppan_dict_list, len_dict = parse_peppan(peppan_genes, peppan_encode)
-	else:
+	if peppan_infile != None:
 		peppan_dict_list, len_dict = compare_peppan(peppan_infile, ignore_pseudogenes)
-	
-	# iterate over peppan inputs and calculate statistics
-	stat_list_IQR = []
-	stat_list_prop_IQR = []
-	stat_list_MAD = []
-	stat_list_prop_MAD = []
-	stat_list_prop_stdev = []
-	stat_list_stdev = []
-	stat_size_cluster = []
 
-	# get stats per cluster size
-	stat_list_IQR_size = []
-	stat_list_MAD_size = []
-	stat_list_stdev_size = []
+		# iterate over peppan inputs and calculate statistics
+		stat_list_IQR = []
+		stat_list_prop_IQR = []
+		stat_list_MAD = []
+		stat_list_prop_MAD = []
+		stat_list_prop_stdev = []
+		stat_list_stdev = []
+		stat_size_cluster = []
 
-	for cluster_id, ORF_sizes in peppan_dict_list.items():
-		ORF_sizes = np.array(ORF_sizes).astype(int)
-		cluster_size = ORF_sizes.size
-		if cluster_size == 1 and ignore_singletons:
-			continue
+		# get stats per cluster size
+		stat_list_IQR_size = []
+		stat_list_MAD_size = []
+		stat_list_stdev_size = []
 
-		stat_size_cluster.append(cluster_size)
-		q75, q50, q25 = np.percentile(ORF_sizes, [75, 50, 25])
+		for cluster_id, ORF_sizes in peppan_dict_list.items():
+			ORF_sizes = np.array(ORF_sizes).astype(int)
+			cluster_size = ORF_sizes.size
+			if cluster_size == 1 and ignore_singletons:
+				continue
 
-		if cluster_size == 1:
-			stat_list_MAD.append(0.0)
-			stat_list_prop_MAD.append(0.0)
-			stat_list_MAD_size.append(0.0)
-		else:
-			stat_list_MAD.append(scipy.stats.median_abs_deviation(ORF_sizes))
-			stat_list_MAD_size.append(scipy.stats.median_abs_deviation(ORF_sizes) / cluster_size)
-			stat_list_prop_MAD.append(scipy.stats.median_abs_deviation(ORF_sizes) / q50)
+			stat_size_cluster.append(cluster_size)
+			q75, q50, q25 = np.percentile(ORF_sizes, [75, 50, 25])
 
-		stat_list_IQR.append(float(q75 - q25))
-		stat_list_IQR_size.append(float((q75 - q25) / cluster_size))
-		stat_list_prop_IQR.append(float((q75 - q25) / q50))
+			if cluster_size == 1:
+				stat_list_MAD.append(0.0)
+				stat_list_prop_MAD.append(0.0)
+				stat_list_MAD_size.append(0.0)
+			else:
+				stat_list_MAD.append(scipy.stats.median_abs_deviation(ORF_sizes))
+				stat_list_MAD_size.append(scipy.stats.median_abs_deviation(ORF_sizes) / cluster_size)
+				stat_list_prop_MAD.append(scipy.stats.median_abs_deviation(ORF_sizes) / q50)
 
-		stat_list_prop_stdev.append(float(np.std(ORF_sizes)) / float(np.mean(ORF_sizes)))
-		stat_list_stdev.append(float(np.std(ORF_sizes)))
-		stat_list_stdev_size.append(float(np.std(ORF_sizes) / cluster_size))
+			stat_list_IQR.append(float(q75 - q25))
+			stat_list_IQR_size.append(float((q75 - q25) / cluster_size))
+			stat_list_prop_IQR.append(float((q75 - q25) / q50))
 
-	# convert to numpy arrays
-	stat_list_IQR = np.array(stat_list_IQR)
-	stat_list_IQR_size = np.array(stat_list_IQR_size)
-	stat_list_prop_IQR = np.array(stat_list_prop_IQR)
+			stat_list_prop_stdev.append(float(np.std(ORF_sizes)) / float(np.mean(ORF_sizes)))
+			stat_list_stdev.append(float(np.std(ORF_sizes)))
+			stat_list_stdev_size.append(float(np.std(ORF_sizes) / cluster_size))
 
-	stat_list_MAD = np.array(stat_list_MAD)
-	stat_list_MAD_size = np.array(stat_list_MAD_size)
-	stat_list_prop_MAD = np.array(stat_list_prop_MAD)
+		# convert to numpy arrays
+		stat_list_IQR = np.array(stat_list_IQR)
+		stat_list_IQR_size = np.array(stat_list_IQR_size)
+		stat_list_prop_IQR = np.array(stat_list_prop_IQR)
 
-	stat_list_stdev = np.array(stat_list_stdev)
-	stat_list_stdev_size = np.array(stat_list_stdev_size)
-	stat_list_prop_stdev = np.array(stat_list_prop_stdev)
+		stat_list_MAD = np.array(stat_list_MAD)
+		stat_list_MAD_size = np.array(stat_list_MAD_size)
+		stat_list_prop_MAD = np.array(stat_list_prop_MAD)
 
-	stat_size_cluster = np.array(stat_size_cluster)
+		stat_list_stdev = np.array(stat_list_stdev)
+		stat_list_stdev_size = np.array(stat_list_stdev_size)
+		stat_list_prop_stdev = np.array(stat_list_prop_stdev)
 
-	combined_stdev = np.column_stack((stat_list_prop_stdev, stat_list_stdev, stat_list_stdev_size))
-	combined_IQR = np.column_stack((stat_list_prop_IQR, stat_list_IQR, stat_list_IQR_size))
-	combined_MAD = np.column_stack((stat_list_prop_MAD, stat_list_MAD, stat_list_MAD_size))
+		stat_size_cluster = np.array(stat_size_cluster)
 
-	np.savetxt(outpref + "_peppan_stddev.txt", combined_stdev, delimiter=",")
-	np.savetxt(outpref + "_peppan_IQR.txt", combined_IQR, delimiter=",")
-	np.savetxt(outpref + "_peppan_MAD.txt", combined_MAD, delimiter=",")
-	np.savetxt(outpref + "_peppan_sizes.txt", stat_size_cluster, delimiter=",")
+		combined_stdev = np.column_stack((stat_list_prop_stdev, stat_list_stdev, stat_list_stdev_size))
+		combined_IQR = np.column_stack((stat_list_prop_IQR, stat_list_IQR, stat_list_IQR_size))
+		combined_MAD = np.column_stack((stat_list_prop_MAD, stat_list_MAD, stat_list_MAD_size))
 
-	if verbose:
-		print("Input: " + peppan_infile)
-		print("Average prop. standard deviation: {}".format(np.mean(stat_list_prop_stdev)))
-		print("Stdev prop. standard deviation: {}".format(np.std(stat_list_prop_stdev)))
-		print("Average standard deviation: {}".format(np.mean(stat_list_stdev)))
-		print("Stdev standard deviation: {}".format(np.std(stat_list_stdev)))
-		print("Average standard deviation (size adjusted): {}".format(np.mean(stat_list_stdev_size)))
-		print("Stdev standard deviation (size adjusted: {}".format(np.std(stat_list_stdev_size)))
+		np.savetxt(outpref + "_peppan_stddev.txt", combined_stdev, delimiter=",")
+		np.savetxt(outpref + "_peppan_IQR.txt", combined_IQR, delimiter=",")
+		np.savetxt(outpref + "_peppan_MAD.txt", combined_MAD, delimiter=",")
+		np.savetxt(outpref + "_peppan_sizes.txt", stat_size_cluster, delimiter=",")
 
-		print("Average prop. IQR: {}".format(np.mean(stat_list_prop_IQR)))
-		print("Stdev prop. IQR: {}".format(np.std(stat_list_prop_IQR)))
-		print("Average IQR: {}".format(np.mean(stat_list_IQR)))
-		print("Stdev IQR: {}".format(np.std(stat_list_IQR)))
-		print("Average IQR (size adjusted): {}".format(np.mean(stat_list_IQR_size)))
-		print("Stdev IQR (size adjusted): {}".format(np.std(stat_list_IQR_size)))
+		if verbose:
+			print("Input: " + peppan_infile)
+			print("Average prop. standard deviation: {}".format(np.mean(stat_list_prop_stdev)))
+			print("Stdev prop. standard deviation: {}".format(np.std(stat_list_prop_stdev)))
+			print("Average standard deviation: {}".format(np.mean(stat_list_stdev)))
+			print("Stdev standard deviation: {}".format(np.std(stat_list_stdev)))
+			print("Average standard deviation (size adjusted): {}".format(np.mean(stat_list_stdev_size)))
+			print("Stdev standard deviation (size adjusted: {}".format(np.std(stat_list_stdev_size)))
 
-		print("Average prop. MAD: {}".format(np.mean(stat_list_prop_MAD)))
-		print("Stdev prop. MAD: {}".format(np.std(stat_list_prop_MAD)))
-		print("Average MAD: {}".format(np.mean(stat_list_MAD)))
-		print("Stdev MAD: {}".format(np.std(stat_list_MAD)))
-		print("Average MAD (size adjusted): {}".format(np.mean(stat_list_MAD_size)))
-		print("Stdev MAD (size adjusted): {}".format(np.std(stat_list_MAD_size)))
+			print("Average prop. IQR: {}".format(np.mean(stat_list_prop_IQR)))
+			print("Stdev prop. IQR: {}".format(np.std(stat_list_prop_IQR)))
+			print("Average IQR: {}".format(np.mean(stat_list_IQR)))
+			print("Stdev IQR: {}".format(np.std(stat_list_IQR)))
+			print("Average IQR (size adjusted): {}".format(np.mean(stat_list_IQR_size)))
+			print("Stdev IQR (size adjusted): {}".format(np.std(stat_list_IQR_size)))
 
-		print("Average cluster size: {}".format(np.mean(stat_size_cluster)))
-		print("Stdev cluster size: {}".format(np.std(stat_size_cluster)))
+			print("Average prop. MAD: {}".format(np.mean(stat_list_prop_MAD)))
+			print("Stdev prop. MAD: {}".format(np.std(stat_list_prop_MAD)))
+			print("Average MAD: {}".format(np.mean(stat_list_MAD)))
+			print("Stdev MAD: {}".format(np.std(stat_list_MAD)))
+			print("Average MAD (size adjusted): {}".format(np.mean(stat_list_MAD_size)))
+			print("Stdev MAD (size adjusted): {}".format(np.std(stat_list_MAD_size)))
+
+			print("Average cluster size: {}".format(np.mean(stat_size_cluster)))
+			print("Stdev cluster size: {}".format(np.std(stat_size_cluster)))
 
 	if roary_infile != None:
 		with open(len_dict_file, "r") as f:
